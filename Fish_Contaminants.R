6f03b2713d5e68779a1f0629bbd3ac757e86b7fa
Kjones-90 <kjones26@uoguelph.ca>
2024-10-08 13:14:38 -0400

    commit3

--- Fish_Contaminants.R
+++ Fish_Contaminants.R
@@ -45,7 +45,7 @@ ggplot(PCB, aes(x=ONTARIO, y=VALUE)) +
 PCBno_out <- filter(PCB, VALUE<35000)
 
 ggplot(PCBno_out, aes(x=ONTARIO, y=VALUE)) +
-  geom_jitter(alpha=0.5) +
+  geom_jitter(alpha=0.1) +
   ylab("PCB (ng/g wet)")
 
 FarNorthHg<-add_column(FarNorthHg, ONTARIO="North")
@@ -58,5 +58,29 @@ ggplot(Hg, aes(x=ONTARIO, y=VALUE)) +
   ylab("Hg (ug/g wet)")
 
 
+GreatLakesTroph2 <- filter(GreatLakesHg, grepl("White Sucker", SPECIES_NAME, fixed = TRUE) | grepl("Spottail Shiner", SPECIES_NAME, fixed = TRUE) | grepl("Emerald Shiner", SPECIES_NAME, fixed = TRUE))
+GreatLakesTroph2 <- add_column(GreatLakesTroph2, TROPHICLVL="2")
+
+GreatLakesTroph3 <- filter(GreatLakesHg, grepl("Common Carp", SPECIES_NAME, fixed = TRUE) | grepl("Rock Bass", SPECIES_NAME, fixed = TRUE) | grepl("Black Crappie", SPECIES_NAME, fixed = TRUE))
+GreatLakesTroph3 <- add_column(GreatLakesTroph3, TROPHICLVL="3")
+
+GreatLakesTroph4 <- filter(GreatLakesHg, grepl("Northern Pike", SPECIES_NAME, fixed = TRUE) | grepl("Rainbow Trout", SPECIES_NAME, fixed = TRUE) | grepl("White Bass", SPECIES_NAME, fixed = TRUE))
+GreatLakesTroph4 <- add_column(GreatLakesTroph4, TROPHICLVL="4")
+
+GreatLakesTroph <- rbind(GreatLakesTroph2, GreatLakesTroph3, GreatLakesTroph4)
+
+ggplot(GreatLakesTroph, aes(x=TROPHICLVL,y=VALUE)) +
+  geom_jitter(alpha=0.1) +
+  xlab("Trophic Level") +
+  ylab("Mercury (ug/g wet)") +
+  scale_x_discrete(labels=c("2"="Primary Consumers", "3"="Secondary Consumers", "4"="Tertiary Consumers"))
+
+
+ggplot(PCB, aes(fill=ONTARIO, y=LENGTH_CM, x=SPECIES_NAME))+
+  geom_bar(position="dodge", stat="identity")
+
+ggplot(GreatLakesPCB, aes(x=VALUE, y=WEIGHT_GRAM))+
+  geom_point()+
+  facet_wrap("SPECIES_NAME")
 
 
