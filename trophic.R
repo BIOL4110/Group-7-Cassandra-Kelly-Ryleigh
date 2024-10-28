5a146f4a3b947f6460b4de0b45c0b485b4096321
Kjones-90 <kjones26@uoguelph.ca>
2024-10-27 22:35:09 -0400

    10-27 again again

--- trophic.R
+++ trophic.R
@@ -11,22 +11,13 @@ species_fbase<-species_fbase %>% select("SpecCode","FBname")
 #merging into one table
 trophic_species <- merge(species_fbase,trophic_fbase, by  = "SpecCode") 
 
-#now what?
-
+#averaging duplicate trophic levels
 Trophic_avg <- trophic_species %>%
   group_by(SpecCode, SampleStage, FBname) %>%
   summarize(TrophicAverage = mean(Troph, na.rm = TRUE), .groups = 'drop')
 
-
-
-
-#Maybe need to tidy the data? Perhaps it is best to just average the trophic levels for each species
-#to make things simpler? (Done above)
-
-
-trophic_species<-na.omit(trophic_species)
-
-trophic_species$FBname<-tolower(trophic_species$FBname)
+#make our two datasets have lower case 
+Trophic_avg$FBname<-tolower(Trophic_avg$FBname)
 
 fish_Hg_PCB$species_name<-tolower(fish_Hg_PCB$species_name)
 
