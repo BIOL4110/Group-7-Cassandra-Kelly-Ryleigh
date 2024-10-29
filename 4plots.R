3bb87e48f1f6fab08379da762f14efa3a163f008
Kjones-90 <kjones26@uoguelph.ca>
2024-10-29 16:35:48 -0400

    10-29

--- 4plots.R
+++ 4plots.R
@@ -1,11 +1,15 @@
-PCB <- filter(trophic_Hg_PCB, grepl("chlorobiphenyl", parameter_name, fixed = TRUE) | 
+PCB <- filter(fish_genus_troph, grepl("chlorobiphenyl", parameter_name, fixed = TRUE) | 
                 grepl("PCB", parameter_name, fixed = TRUE) | 
                 grepl("(Cl)biphenyl", parameter_name, fixed = TRUE))
-Hg<-filter(trophic_Hg_PCB, grepl("Mercury", parameter_name, fixed = TRUE))
+Hg<-filter(fish_genus_troph, grepl("Mercury", parameter_name, fixed = TRUE))
 
 Hg %>% ggplot(aes(TrophicAverage,value))+
-  geom_jitter()+
-  geom_smooth()
+  geom_jitter(alpha=0.1)+
+  ylab("Mercury (ug/g wet)")+
+  xlab("Trophic Level")+
+  theme_bw()+
+  theme(panel.border = element_blank(), panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
 
 Hg %>% ggplot(aes(weight_gram,value))+
   geom_point()+
